ca024dd482329efcfa1eeef87eebb021915e2aa8
WittyDevil <rohith.coder85@gmail.com>
2024-10-05 14:35:26 +0530

    Added Buttons with icons in Navigation Bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,14 +10,16 @@ export default function NavBar()
             </div>
             <div id="navbtnContainer-div">
                 <text className="navItemText">Categories</text>
-                <input className="navIteminput"></input>
+                <input className="navIteminput" placeholder="Search anything"></input>
                 <text className="navItemText">Udemy Business</text>
                 <text className="navItemText">Teach on Udemy</text>
-                <text className="navItemText">My learning</text>
-                <button className="navItemBtn" img="wishlist.svg">Wishlist</button>
-                <button className="navItemBtn" img="">Cart</button>
-                <button className="navItemBtn" img="">Notifications</button>
-                <button className="navItemBtn" img="" >Profile</button>
+                <text className="navItemText">My learning</text>                
+            </div>
+            <div id="navbticon-div">
+                <button className="navItemBtnIcon" ><img src="heart.png" alt="Wishlist"></img></button>
+                <button className="navItemBtnIcon"><img src="cart.png" alt="Add to Cart"></img></button>
+                <button className="navItemBtnIcon"><img src="bell-24.png" alt="Notifications"></img></button>
+                <button className="navItemBtnIcon"><img src="circle-32.png" alt="My Profile"></img></button>
             </div>
         </div>
     );
